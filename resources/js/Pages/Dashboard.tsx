bded81aa2f9d38d9a217675518af493fc18a27cd
Bruno M. Silva <brunosilva010601@gmail.com>
2024-10-23 23:24:03 -0300

    install shadcn-ui

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,11 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Button } from "@/Components/ui/button";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { useState } from "react";
 
 export default function Dashboard() {
+    const [date, setDate] = useState<Date | undefined>(new Date());
+
     return (
         <AuthenticatedLayout
             header={
@@ -17,6 +21,8 @@ export default function Dashboard() {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             You're logged in!
+                            <Button>dwadwa</Button>
+                            <div className="flex p-10"></div>
                         </div>
                     </div>
                 </div>
