ae082603a12c24b688d05fb728e7c6f5c0355a9d
Bruno M. Silva <brunosilva010601@gmail.com>
2024-10-29 11:36:32 -0300

    add axios type

--- resources/js/types/global.d.ts
+++ resources/js/types/global.d.ts
@@ -1,6 +1,12 @@
 import { PageProps as InertiaPageProps } from "@inertiajs/core";
 import { PageProps as AppPageProps } from "./";
 
+declare global {
+    interface Window {
+        axios: AxiosInstance;
+    }
+}
+
 declare module "@inertiajs/core" {
     interface PageProps extends InertiaPageProps, AppPageProps {}
 }
