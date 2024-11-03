adf2fa7790225986c74aaa6c060031eccffdb63e
Bruno M. Silva <brunosilva010601@gmail.com>
2024-11-03 09:29:39 -0300

    wip

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,10 +1,6 @@
-import useFetch from "@/Hooks/useFetch";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { route } from "ziggy-js";
 
 export default function Dashboard() {
-    const fetchAssetOptions = useFetch<App.Data.AssetData[]>(route("assets.get"));
-
     return (
         <AuthenticatedLayout headTitle="Dashboard">
             <div className="space-y-4">
@@ -12,13 +8,6 @@ export default function Dashboard() {
 
                 <div className="p-10 border">
                     <p>Dashboard content goes here</p>
-
-                    <div>{fetchAssetOptions.loading ? "loading" : "done"}</div>
-                    <div>{fetchAssetOptions.error}</div>
-
-                    {fetchAssetOptions.data?.map((each, index) => (
-                        <div key={index}>{each.name}</div>
-                    ))}
                 </div>
             </div>
         </AuthenticatedLayout>
